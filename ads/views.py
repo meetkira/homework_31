ca180b75b9d6d075e16a9a95757a0d15fe2a63f5
Кристина Ратник <kristi.2001@yandex.ru>
2022-08-17 17:26:12 +0300

    fix ads views

--- ads/views.py
+++ ads/views.py
@@ -19,60 +19,10 @@ def index(request):
     return JsonResponse({"status": "ok"}, status=200)
 
 
-'''@method_decorator(csrf_exempt, name="dispatch")
-class AdListView(ListView):
-    model = Ad
-    queryset = Ad.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        super().get(request, *args, **kwargs)
-
-        cats = request.GET.getlist("cats", [])
-        text = request.GET.get("text", None)
-        location = request.GET.get("location", None)
-        price_from = request.GET.get("price_from", None)
-        price_to = request.GET.get("price_to", None)
-
-        if cats:
-            self.object_list = self.object_list.filter(category_id__in=cats)
-        if text:
-            self.object_list = self.object_list.filter(name__icontains=text)
-        if location:
-            self.object_list = self.object_list.filter(author__locations__name__icontains=location)
-        if price_from:
-            self.object_list = self.object_list.filter(price__gte=price_from)
-        if price_to:
-            self.object_list = self.object_list.filter(price__lte=price_to)
-
-        self.object_list = self.object_list.order_by("price")
-
-        paginator = Paginator(self.object_list, settings.TOTAL_ON_PAGE)
-        page_number = request.GET.get("page", 1)
-        page_obj = paginator.get_page(page_number)
-
-        ads = []
-        for ad in page_obj:
-            ads.append({
-                "id": ad.id,
-                "name": ad.name,
-                "price": ad.price,
-                "description": ad.description,
-                "is_published": ad.is_published,
-                "image": ad.image.url if ad.image else None,
-                "author": ad.author.first_name,
-                "category": ad.category.name,
-            })
-
-        response = {
-            "items": ads,
-            "num_pages": paginator.num_pages,
-            "total": paginator.count
-        }
-
-        return JsonResponse(response, safe=False)'''
 class AdListView(ListAPIView):
     queryset = Ad.objects.all()
     serializer_class = AdDetailSerializer
+
 
 class AdCreateView(CreateAPIView):
     queryset = Ad.objects.all()
